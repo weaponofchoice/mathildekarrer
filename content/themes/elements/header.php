d2ff5355d1134a3e503aba0a0787be466d7b7771
Luc Awater <lucawater@gmail.com>
2015-08-06 09:38:56 +0200

    correct last name

--- content/themes/elements/header.php
+++ content/themes/elements/header.php
@@ -8,23 +8,23 @@
 <!--[if IE 9]>    <html class="no-js lt-ie10" lang="en"> <![endif]-->
 <!--[if gt IE 9]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-  <title>Mathilde Karrer</title>
-  
+  <title>Mathilde Karrèr</title>
+
   <!-- META TAGS -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no">
-  
+
   <!-- LINK tags -->
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/app.css">
-  
+
   <!-- Fonts from Typography.com -->
   <link rel="stylesheet" type="text/css" href="//cloud.typography.com/680076/710968/css/fonts.css" />
-  
+
   <!-- Fonts from Fontdeck -->
   <link rel="stylesheet" href="//f.fontdeck.com/s/css/uH5+KWQnibDTJRYggGJ9XZLTAgw/DOMAIN_NAME/55988.css" type="text/css" />
-  
+
   <!-- WP_HEAD() -->
   <?php wp_head(); ?>
 </head>
@@ -33,14 +33,14 @@
   <!-- Header -->
   <header>
     <a href="<?php echo home_url(); ?>">
-      <h1 class="is_bold is_uppercase">Mathilde Karrer</h1>
+      <h1 class="is_bold is_uppercase">Mathilde Karrèr</h1>
       <h1 class="is_italic">Photography</h1>
     </a>
-    
+
     <a class="overlay_open system is_uppercase">About & Contact</a>
     <!-- <a class="trigger trigger-menu"><i></i></a> -->
   </header>
-  
+
   <?php
   // Hero element
   if( get_field( 'hero_banner' ) ):
@@ -48,6 +48,6 @@
     include_once( locate_template('content/hero.php') );
   endif;
   ?>
-  
+
   <!-- Main content -->
   <main role="main">
